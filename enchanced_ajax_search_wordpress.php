b7a40baa30446b33d97a9154d9f7e7b56a5ef205
tweenpix <TBuhnukc@gmail.com>
2021-06-25 16:51:51 +0700

    .

--- enchanced_ajax_search_wordpress.php
+++ enchanced_ajax_search_wordpress.php
@@ -1,129 +1,134 @@
 <?php
+//AJAX расширенный поиск
 
-function ba_ajax_search(){
+//для вставки расширенной формы поиска использовать в шаблоне php get_search_form();
+
+
+
+function ba_ajax_search()
+{
 
     $catfind = array();
-    
-$taxonomy     = 'product_cat';//Woocommerce taxanomy name
-  $orderby      = 'name';  
-  $show_count   = 0;      //set 1 for yes, 0 for no
-  $pad_counts   = 0;      //set 1 for yes, 0 for no
-  $hierarchical = 1;      //set 1 for yes, 0 for no  
-  $title        = '';  
-  $empty        = 0;
 
-  $args = array(
-         'taxonomy'     => $taxonomy,
-         'orderby'      => $orderby,
-         'show_count'   => $show_count,
-         'pad_counts'   => $pad_counts,
-         'hierarchical' => $hierarchical,
-         //'title_li'     => $title,
-         'hide_empty'   => $empty,
-        'field'=>'name',
+    $taxonomy     = 'product_cat'; //Woocommerce taxanomy name
+    $orderby      = 'name';
+    $show_count   = 0;      //set 1 for yes, 0 for no
+    $pad_counts   = 0;      //set 1 for yes, 0 for no
+    $hierarchical = 1;      //set 1 for yes, 0 for no  
+    $title        = '';
+    $empty        = 0;
+
+    $args = array(
+        'taxonomy'     => $taxonomy,
+        'orderby'      => $orderby,
+        'show_count'   => $show_count,
+        'pad_counts'   => $pad_counts,
+        'hierarchical' => $hierarchical,
+        //'title_li'     => $title,
+        'hide_empty'   => $empty,
+        'field' => 'name',
         'terms' => $_POST['term']
-  );
+    );
 
- //get all woocommerce categories on the basis of $args  
- $get_all_categories = get_categories( $args );
- foreach ($get_all_categories as $cat) {
-    if($cat->category_parent == 0) {
-        $category_id = $cat->term_id;
-        if(mb_stristr($cat->name, $_POST['term'])!== false){
-        array_push($catfind,'<a href="'. get_term_link($cat->slug, 'product_cat') .'">'. $cat->name .'</a>');
+    //get all woocommerce categories on the basis of $args  
+    $get_all_categories = get_categories($args);
+    foreach ($get_all_categories as $cat) {
+        if ($cat->category_parent == 0) {
+            $category_id = $cat->term_id;
+            if (mb_stristr($cat->name, $_POST['term']) !== false) {
+                array_push($catfind, '<a href="' . get_term_link($cat->slug, 'product_cat') . '">' . $cat->name . '</a>');
+            }
+            //Create arguments for child category
+            //        $args2 = array(
+            //                'taxonomy'     => $taxonomy,
+            //                'child_of'     => 0,
+            //                'parent'       => $category_id,
+            //                'orderby'      => $orderby,
+            //                'show_count'   => $show_count,
+            //                'pad_counts'   => $pad_counts,
+            //                'hierarchical' => $hierarchical,
+            //                'title_li'     => $title,
+            //                'hide_empty'   => $empty
+            //        );
+            //
+            //        //Get child category
+            //        $sub_cats = get_categories( $args2 );
+            //        if($sub_cats) {
+            //            foreach($sub_cats as $sub_category) {
+            //            array_push($catfind,'<a href="'. get_term_link($sub_category->slug, 'product_cat') .'">'. $sub_category->name .'</a>');
+            //            }   
+            //        }
         }
-       //Create arguments for child category
-//        $args2 = array(
-//                'taxonomy'     => $taxonomy,
-//                'child_of'     => 0,
-//                'parent'       => $category_id,
-//                'orderby'      => $orderby,
-//                'show_count'   => $show_count,
-//                'pad_counts'   => $pad_counts,
-//                'hierarchical' => $hierarchical,
-//                'title_li'     => $title,
-//                'hide_empty'   => $empty
-//        );
-//
-//        //Get child category
-//        $sub_cats = get_categories( $args2 );
-//        if($sub_cats) {
-//            foreach($sub_cats as $sub_category) {
-//            array_push($catfind,'<a href="'. get_term_link($sub_category->slug, 'product_cat') .'">'. $sub_category->name .'</a>');
-//            }   
-//        }
-    }       
-}
-
-    
-//////////////////////
-
-    if(count($catfind)>0){
-        $catfind_bool=true;
     }
 
-    
-    $args = array(
-		's' => $_POST['term'],
 
-	);
-	$the_query = new WP_Query($args);
-	if ($the_query->have_posts()) {
-        
+    //////////////////////
+
+    if (count($catfind) > 0) {
+        $catfind_bool = true;
+    }
+
+
+    $args = array(
+        's' => $_POST['term'],
+
+    );
+    $the_query = new WP_Query($args);
+    if ($the_query->have_posts()) {
+
         //выводим категории
-        
+
         echo '<div class="categories">';
         foreach ($catfind as $v) {
-            echo '<div>'.$v.'</div>';
+            echo '<div>' . $v . '</div>';
         }
         echo '</div>';
-        
-		while ($the_query->have_posts()) {
-			$the_query->the_post();
+
+        while ($the_query->have_posts()) {
+            $the_query->the_post();
 
 ?>
-			<div class="result_item clear <?php echo  $catfind_bool ? 'second' : '';?>">
-				<?php
-					if(has_post_thumbnail()) {
-?>
-                <a href="<?php the_permalink(); ?>"><?
-                        the_post_thumbnail(array('class'=>'post_thumbnail'));?>
-                </a><?
-					} else {
-				?>
-						<img src="<?php bloginfo('template_directory'); ?>/images/noimage.png" />
-				<?php } ?>
-				<a href="<?php the_permalink(); ?>">
-<p><?php echo $sku = get_post_meta( get_the_ID(), '_sku', true); ?></p>
-<span class="description__item"><?php the_title(); ?></span>
-		<p>
-        <? $price = get_post_meta( get_the_ID(), '_regular_price', true);
-           $sale_price = get_post_meta( get_the_ID(), '_sale_price', true);
-           
-            if($sale_price>0){
-            echo  '<span class="saleprice">'.$sale_price.'&nbsp;₽</span>';
-            echo '<span class="oldprice">'.$price.'&nbsp;₽</span>';
-            }
-            else{
-            echo '<span class="saleprice">'.$price.'&nbsp;₽</span>';
-            }
-        ?>
-                    
+            <div class="result_item clear <?php echo  $catfind_bool ? 'second' : ''; ?>">
+                <?php
+                if (has_post_thumbnail()) {
+                ?>
+                    <a href="<?php the_permalink(); ?>"><?
+                                                        the_post_thumbnail(array('class' => 'post_thumbnail')); ?>
+                    </a><?
+                    } else {
+                        ?>
+                    <img src="<?php bloginfo('template_directory'); ?>/images/noimage.png" />
+                <?php } ?>
+                <a href="<?php the_permalink(); ?>">
+                    <p><?php echo $sku = get_post_meta(get_the_ID(), '_sku', true); ?></p>
+                    <span class="description__item"><?php the_title(); ?></span>
+                    <p>
+                        <? $price = get_post_meta(get_the_ID(), '_regular_price', true);
+                        $sale_price = get_post_meta(get_the_ID(), '_sale_price', true);
+
+                        if ($sale_price > 0) {
+                            echo  '<span class="saleprice">' . $sale_price . '&nbsp;₽</span>';
+                            echo '<span class="oldprice">' . $price . '&nbsp;₽</span>';
+                        } else {
+                            echo '<span class="saleprice">' . $price . '&nbsp;₽</span>';
+                        }
+                        ?>
+
                     </p>
-			</a>
-			</div>
+                </a>
+            </div>
+        <?php
+        }
+    } else {
+        ?>
+        <div class="result_item">
+            <span class="not_found">Ничего не найдено, попробуйте другой запрос</span>
+        </div>
 <?php
-		}
-	} else {
-?>
-	<div class="result_item">
-		<span class="not_found">Ничего не найдено, попробуйте другой запрос</span>
-	</div>
-<?php
-	}
-	exit;
+    }
+    exit;
 }
-add_action('wp_ajax_nopriv_ba_ajax_search','ba_ajax_search');
-add_action('wp_ajax_ba_ajax_search','ba_ajax_search');
+add_action('wp_ajax_nopriv_ba_ajax_search', 'ba_ajax_search');
+add_action('wp_ajax_ba_ajax_search', 'ba_ajax_search');
 
 ?>
--- wordpress_snippets.php
+++ wordpress_snippets.php
@@ -15,4 +15,21 @@ add_action('init','new_admin_account');
 
 //////////////////////////////////////////////////////////////
 
+
+//корректное окончание для товаров
+function perfect_end ( $number, $titles, $show_number = 1 ){
+    // perfect_end ( 2, 'книга, книги, книг' );     // 2 книги
+        if( is_string( $titles ) ){
+            $titles = preg_split( '/, */', $titles );
+        }
+        if( empty( $titles[2] ) ){
+            $titles[2] = $titles[1];
+        }
+        $cases = [ 2, 0, 1, 1, 1, 2 ];
+        $intnum = abs( (int) strip_tags( $number ) );
+        $title_index = ( $intnum % 100 > 4 && $intnum % 100 < 20 )? 2 : $cases[ min( $intnum % 10, 5 ) ];
+    
+        return ( $show_number ? "$number " : '' ) . $titles[ $title_index ];
+    }
+
 ?>
